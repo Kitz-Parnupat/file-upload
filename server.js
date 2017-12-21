f40b591d56d69ed6fe431239f1cadc87dba9ce7a
Parnupat Jansiri <dainokung2535@gmail.com>
2017-12-21 10:04:05 +0700

    upload file

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 var express = require('express');
+var multer = require('multer');
+var fs = require('fs');
+
 var app = express();
+app.use(multer({
+    dest: __dirname + '/uploads/',
+    rename: function(fieldname, filename) {
+        return Date.now();
+    },
+    limits: {
+        fileSize: 100000
+    },
+    onFileSizeLimit: function(file) {
+        console.log('Failed: ' + file.originalname + ' is limited');
+        fs.unlink(file.path);
+    }
+}));
 
 app.get('/',function(req,res){
     res.sendFile(__dirname + '/index.html');
